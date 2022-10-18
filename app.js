04398a93860c2a1d109e96d2d32164d2cc58efcd
Rous-99 <rous.bf.10@gmail.com>
2022-10-18 11:51:35 +0200

    barra navegacion arreglada

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 
 /*Cuando hago click en la etiqueta hero__menu añado y quito la clase a nav para mostrar o no el menu*/
 
-const button=document.querySelector('.hero__button')
-const nav=document.querySelector('.hero__nav')
+const button=document.querySelector('.nav__button')
+const nav=document.querySelector('.nav__ul')
 
 button.addEventListener('click', ()=>{
     nav.classList.toggle('no-activo')
